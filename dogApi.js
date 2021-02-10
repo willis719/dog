e4f95e944492fc8aeb97897f0d30095040b26402
Willie Rose <willierose87@gmail.com>
2021-02-10 15:55:32 -0600

    Completed AJAX exercise

--- dogApi.js
+++ dogApi.js
@@ -1,3 +1,15 @@
+function replaceImage(imageUrl) {
+    const dogImg = document.createElement("img");
+    dogImg.setAttribute("src", imageUrl)
+    document.querySelector("body").appendChild(dogImg)
+    dogButton.innerHTML = "Generate Doggo..."
+    console.log(data)
+}
+
+
+
+const dogButton = document.getElementById("dogButton")
+
 
 dogButton.addEventListener("click", function() {
     dogButton.innerHTML = "Generating Doggo..."
@@ -6,13 +18,49 @@ dogButton.addEventListener("click", function() {
         return res.json()
     })
     .then((data) => {
-        const dogImg = document.createElement("img");
-        dogImg.setAttribute("src", data.message)
-        document.querySelector("body").appendChild(dogImg)
-        dogButton.innerHTML = "Generate Doggo..."
-        console.log(data)
+        replaceImage(data.message)
     })
     .catch((error) => {
         console.log(error);
     })
 })
+
+const select = document.getElementById("dogSelect")
+
+fetch("https://dog.ceo/api/breeds/list")
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+
+    for(let i = 0; i < data.message.length; i++) {
+        const name = data.message[i]
+        const option = document.createElement("option")
+        option.innerText = name
+        select.append(option)
+    }
+})
+
+select.addEventListener("change", () => {
+    console.log(select.value)
+
+    fetch(`https://dog.ceo/api/breed/${select.value}/images/random`)
+    .then(res => res.json())
+    .then(data => {
+        replaceImage(data.message)
+})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
